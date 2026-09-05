8d76ae095dea9745a5b952feebe063051c12fb6b
Leo Gomes <106993889+leo-gomes-dev@users.noreply.github.com>
2026-09-05 12:08:12 -0300

    finished game exemplo

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 
 const jump = () => {
-  mario.classList("jump");
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
 };
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = window.getComputedStyle(mario).bottom.replace("px", "");
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    clouds.style.animation = "none";
+
+    mario.src = "./img/game-over.png";
+    mario.style.left = "75px";
+    mario.style.width = "50px";
+
+    clearInterval(loop);
+  }
+}, 10);
 
 document.addEventListener("keydown", jump);
